6088612fc413d4d8622bbcaa410dbd79c913cf36
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-04-03 14:04:27 +0300

    add poka chto ploho rabotauwee nahojdenie korney polinoma

--- Метод Крылова/Source.cpp
+++ Метод Крылова/Source.cpp
@@ -15,6 +15,8 @@ double x[n][n];
 double q1[n][n+1];
 int N = n + 1;
 double p1[n];
+const double eps = 0.0001, delta = 0.001, Min = -100, Max = 100, H = 0.001;
+double K_Pol[n], S_korn[n];
 
 
 void Gauss(int k, double Matrix[n][n + 1]) {
@@ -82,6 +84,41 @@ double ZagatovkaDlyaGaussa() {
 	return 1;
 }
 
+//значение полинома в точке xf
+double Pol(double* A, double xf) {
+	double Sum = pow(xf, N);
+	for (size_t i = 1; i <= N; i++) {
+		Sum -= K_Pol[i - 1] * pow(xf, N - i);
+	}
+	return pow(-1, N) * Sum;
+}
+//производная полинома в точке xf
+double dx(double xf) {
+	return (Pol(K_Pol, xf + delta) - Pol(K_Pol, xf - delta)) / 2 / delta;
+}
+
+//помещаем начальное значение, получаем уточненное значение
+double Nyuton(double x_0) {
+	double Mod = 1, xi = 0;
+	while (eps < Mod) {
+		xi = x_0 - Pol(K_Pol, x_0) / dx(x_0);
+		Mod = fabs(xi - x_0);
+		x_0 = xi;
+	}
+	return xi;
+}
+
+//отделение корней, помещает примерное значение корней
+void Search(double* A) {
+	int i = 0;
+	for (double t = Min; t < Max; t += H) {
+		if (Pol(K_Pol, t) * Pol(K_Pol, t + H) < 0) {
+			A[i] = t + H / 2;
+			i++;
+		}
+	}
+}
+
 
 int main() {
 
@@ -95,6 +132,7 @@ int main() {
 	{
 		p1[i] = q1[i][4];
 	}
+
 	// C помощью метода гауса нашли все p, они равны соответственно	
 	double p[n] = { 6,0.2,-12.735,2.7616 };
 	//имея эти значения можем найти Лямбды
@@ -165,7 +203,15 @@ int main() {
 		cout << "\n";
 	}
 
-	
+	for (size_t i = 0; i < n; i++) {
+		K_Pol[i] = p[i];
+	}
+
+	Search(S_korn);
+	for (size_t i = 0; i < n; i++) {
+		cout << endl << Nyuton(S_korn[i]) << endl;
+		cout << p1[i] << endl;
+	}
 
 	return 1;
 }
