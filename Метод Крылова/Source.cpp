41d04639e2d96d8f6780b8fbc14b3903ba993238
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-04-03 14:07:35 +0300

    delete liwnuu peremennuu

--- Метод Крылова/Source.cpp
+++ Метод Крылова/Source.cpp
@@ -16,7 +16,7 @@ double q1[n][n+1];
 int N = n + 1;
 double p1[n];
 const double eps = 0.0001, delta = 0.001, Min = -100, Max = 100, H = 0.001;
-double K_Pol[n], S_korn[n];
+double  S_korn[n];
 
 
 void Gauss(int k, double Matrix[n][n + 1]) {
@@ -88,20 +88,20 @@ double ZagatovkaDlyaGaussa() {
 double Pol(double* A, double xf) {
 	double Sum = pow(xf, N);
 	for (size_t i = 1; i <= N; i++) {
-		Sum -= K_Pol[i - 1] * pow(xf, N - i);
+		Sum -= p1[i - 1] * pow(xf, N - i);
 	}
 	return pow(-1, N) * Sum;
 }
 //производная полинома в точке xf
 double dx(double xf) {
-	return (Pol(K_Pol, xf + delta) - Pol(K_Pol, xf - delta)) / 2 / delta;
+	return (Pol(p1, xf + delta) - Pol(p1, xf - delta)) / 2 / delta;
 }
 
 //помещаем начальное значение, получаем уточненное значение
 double Nyuton(double x_0) {
 	double Mod = 1, xi = 0;
 	while (eps < Mod) {
-		xi = x_0 - Pol(K_Pol, x_0) / dx(x_0);
+		xi = x_0 - Pol(p1, x_0) / dx(x_0);
 		Mod = fabs(xi - x_0);
 		x_0 = xi;
 	}
@@ -112,7 +112,7 @@ double Nyuton(double x_0) {
 void Search(double* A) {
 	int i = 0;
 	for (double t = Min; t < Max; t += H) {
-		if (Pol(K_Pol, t) * Pol(K_Pol, t + H) < 0) {
+		if (Pol(p1, t) * Pol(p1, t + H) < 0) {
 			A[i] = t + H / 2;
 			i++;
 		}
@@ -203,14 +203,10 @@ int main() {
 		cout << "\n";
 	}
 
-	for (size_t i = 0; i < n; i++) {
-		K_Pol[i] = p[i];
-	}
-
 	Search(S_korn);
 	for (size_t i = 0; i < n; i++) {
 		cout << endl << Nyuton(S_korn[i]) << endl;
-		cout << p1[i] << endl;
+		//cout << p1[i] << endl;
 	}
 
 	return 1;
